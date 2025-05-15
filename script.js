cd97202954ca759c99b00d1875292af24243732b
alvieg <168017188+alvieg@users.noreply.github.com>
2025-05-14 19:38:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const exampleLinks = [
-  "thegportal.neocities.org",
-  "burgerawesome.neocities.org",
-];
-const list = document.getElementById('links');
-//<li>thegportal.neocities.org</li>
-const listItems = document.querySelectorAll('#links li');
-const inputBar = document.getElementById('urlbox');
-function addToBar(TEXT) {
-  var clickedItem = document.getElementById(TEXT)
+
+const gameButtons = {
+"GHUB":"https://thegportal.neocities.org",
+"Burger Awesome":"https://burgerawesome.neocities.org",
+"HaHa Games":"https://hahagames.com",
+"Butter Dog Games":"https://teachers-are-the-best.neocities.org",
+"GN-Math":"https://gn-math.github.io"
+}
+const gameSpace = document.getElementById('gamebuttons')
+
+for (const [key, value] of Object.entries(gameButtons)) {
+  const button = document.createElement("button");
+  button.textContent = key
+  button.onclick = () => redirect(value)
+  gameSpace.appendChild(button)
 }
 
-exampleLinks.forEach(exampleLinks => {
-      const listItem = document.createElement('li'); // Create a new <li> element
-      listItem.textContent = exampleLinks; // Set the text content
-      listItem.onclick = () => addToBar(exampleLinks)
-      list.appendChild(listItem); // Append the <li> to the <ul>
-});
 function checkHTTP(VAR) {
 if (!VAR.startsWith('https://')) {
 VAR = "https://" + VAR;
@@ -31,6 +30,9 @@ iframe.style.width = '100%';
 iframe.style.height = '100%';
 iframe.style.margin = '0';
 var url = document.getElementById('urlbox').value;
+if (url == "") {
+  alert('No URL Entered')
+} else {
 if (!url.startsWith('https://')) {
 url = "https://" + url;
 };
@@ -42,6 +44,23 @@ win.document.body.appendChild(script);
 window.close();
 console.log(url)
 }
-setTimeout(() => {
-openInNewTab("${url}");
-}, 2500);
+}
+
+function redirect(URL) {
+var win = window.open();
+win.document.body.style.margin = '0';
+win.document.body.style.height = '100vh';
+var iframe = win.document.createElement('embed');
+iframe.style.border = 'none';
+iframe.style.width = '100%';
+iframe.style.height = '100%';
+iframe.style.margin = '0';
+var url = URL;
+iframe.src = url;
+win.document.body.appendChild(iframe);
+var script = win.document.createElement('script');
+script.innerHTML = "setTimeout(() => { location.reload(true); }, 10800 * 1000);";
+win.document.body.appendChild(script);
+window.close();
+console.log(url)
+}
